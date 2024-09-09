e581461e4d394850759b44742292659b63a7bb21
Augustus Paddock <au.paddock@gmail.com>
2024-09-09 12:03:36 -0500

    added button, modified css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = (props) => {
+  const onClick = () => {
+    console.log("button clicked")
+    }
   return (
-    <header>
+    <header className='header'>
         <h1>Make a post, {props.title}</h1>
+        <Button color="green" text="post" onClick={onClick}/>
     </header>
   )
 }
