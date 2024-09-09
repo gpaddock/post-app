635884e8d0cf3d2a33eae4d12f11329b7e834655
Augustus Paddock <au.paddock@gmail.com>
2024-09-09 14:32:11 -0500

    final working build

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import Header from "./components/Header"
 import Posts from "./components/Posts"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import MakePost from "./components/MakePost"
 
 function App() {
   const [showMakePost, setShowMakePost] = useState(false)
 
-  const [posts, setPosts] = useState(    [{
-    id: 1,
-    user: "Gus",
-    city: "Chicago, IL",
-    content:"First Post!",
-    time: new Date(Date.now())
-    },
-    {
-    id:2,
-    user: "Ethan",
-    city: "San Francisco, CA",
-    content:"Second Post!",
-    time: new Date(Date.now()+5000)
-},{
-    id:3,
-    user: "Gus",
-    city: "Chicago, IL",
-    content:"Third Post!",
-    time: new Date(Date.now()+10000)
-}])
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const getPosts = async () => {
+      const postsFromServer = await fetchPosts()
+      setPosts(postsFromServer)
+    }
+    getPosts()
+  }, [])
+  // fetch posts
+  const fetchPosts = async () =>{
+    const res = await fetch("http://localhost:4000/posts")
+    const data = await res.json()
+    return data
+  }
   // add post
-  const addPost = (post) =>{
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newPost = {id, ...post}
-    console.log(newPost)
-    setPosts([...posts, newPost])
+  const addPost = async (post) =>{
+    const res = await fetch('http://localhost:4000/posts', {
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    const data = await res.json()
+
+    setPosts([...posts, data])
+    // old method
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newPost = {id, ...post}
+    // console.log(newPost)
+    // setPosts([...posts, newPost])
   }
   // remove post
-  const deletePost = (id) => {
+  const deletePost = async (id) => {
+    await fetch(`http://localhost:4000/posts/${id}`, {
+      method: 'DELETE'
+    })
     setPosts(posts.filter((post) => post.id !== id))
   }
 
